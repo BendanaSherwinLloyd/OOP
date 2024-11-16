2572c56b3120ddb8be580a1ccfa6242cc1ddd16f
BendanaSherwinLloyd <23-16733@g.batstate-u.edu.ph>
2024-11-16 09:06:57 +0800

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,34 @@
+abstract class Shape {
+    protected String color; 
+    protected Boolean filled; 
+
+    public Shape(String color, Boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(Boolean filled) {
+        this.filled = filled;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+
+    public Boolean isFilled() {
+        return filled;
+    }
+
+    abstract Double getArea();
+
+    abstract Double getPerimeter();
+
+    @Override
+    public String toString() {
+        return "Shape[color=" + color + ", filled=" + (filled ? "filled" : "not filled") + ".";
+    }
+}
